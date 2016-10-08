bf3f2a08cc6bea0451b5370a1a5301111e18c18b
psvmc <183518918@qq.com>
2016-10-08 16:10:26 +0800

    修改信息

--- src/main/java/com/controller/ht/TArticleController.java
+++ src/main/java/com/controller/ht/TArticleController.java
@@ -12,6 +12,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.jsoup.Jsoup;
+import org.jsoup.UnsupportedMimeTypeException;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
@@ -25,27 +26,31 @@ public class TArticleController extends Controller {
 	String baseUrl = "http://www.guancheng.gov.cn/viewCmsCac.do";
 	String fileBasePath;
 	String cacId;
+	int pid = 101;// 分类ID
 
 	private ArticleVo getDetail(ArticleVo articleVo) {
 		Document doc;
 
 		try {
-			System.out.println("抓取的详情页URL:" + articleVo.getUrl());
 			doc = Jsoup.connect(articleVo.getUrl()).get();
-			Elements titleEle = doc.getElementsByAttributeValue("class",
-					"title7");
-			Elements contentEle = doc
-					.getElementsByAttributeValue("class", "co");
+			Elements titleEle = doc.getElementsByAttributeValue("class", "title7");
+			Elements contentEle = doc.getElementsByAttributeValue("class", "co");
+			Elements sourceElements = doc.getElementsContainingText("来源");
+			String sourceStr = "";
+			if (sourceElements.size() == 0) {
 
-			String sourceStr = doc.getElementsContainingText("来源").last()
-					.html();
-			sourceStr = sourceStr.replace("来源：", "")
-					.replace("&nbsp;&nbsp;", "");
+			} else {
+				sourceStr = sourceElements.last().html();
+				sourceStr = sourceStr.replace("来源：", "").replace("&nbsp;&nbsp;", "");
+			}
+
+			if (contentEle.size() == 0) {
+				contentEle = doc.getElementsByTag("table").first().getElementsByTag("tr").get(3).getElementsByTag("td");
+			}
 
 			String addDateStr = articleVo.getAddDate();
 			String addDateStr2 = addDateStr.replace("-", "");
-			String filePath001 = "webpic/W0" + addDateStr2.substring(0, 6)
-					+ "/W" + addDateStr2 + "/W";
+			String filePath001 = "webpic/W0" + addDateStr2.substring(0, 6) + "/W" + addDateStr2 + "/W";
 			Elements imageEles = contentEle.get(0).getElementsByTag("img");
 			for (Element imageEle : imageEles) {
 				String imageUrl = doc.baseUri() + imageEle.attr("src");
@@ -56,23 +61,18 @@ public class TArticleController extends Controller {
 				String oldSrc = ZJ_StringUtils.getFileName(src);
 				imageEle.attr("src", "/" + src);
 				imageEle.attr("oldsrc", oldSrc);
-				System.out.println(imageUrl);
-				System.out.println(filePath);
-
+				System.out.println(" └图片的URL为:" + imageUrl);
+				System.out.println(" └图片保存路径为:" + filePath);
 			}
+			
 			articleVo.setSource(sourceStr);
 			articleVo.setTitle(titleEle.text());
-			String contentEleHtml = contentEle.html()
-					.replaceAll("(\r\n|\r|\n|\n\r)", "<br />")
-					.replaceAll("\'", "\"");
-			String contentEleText = contentEle.text()
-					.replaceAll("(\r\n|\r|\n|\n\r)", "<br />")
-					.replaceAll("\'", "\"");
+			String contentEleHtml = contentEle.html().replaceAll("(\r\n|\r|\n|\n\r)", "<br />").replaceAll("\'", "\"");
+			String contentEleText = contentEle.text().replaceAll("(\r\n|\r|\n|\n\r)", "<br />").replaceAll("\'", "\"");
 			if (contentEleHtml.getBytes("UTF-8").length >= 4000) {
 				if (contentEleText.getBytes("UTF-8").length >= 4000) {
 					articleVo.setContent(subStringByByte(contentEleText, 4000));
-					articleVo.setContentText(subStringByByte(contentEleText,
-							4000));
+					articleVo.setContentText(subStringByByte(contentEleText, 4000));
 				} else {
 					articleVo.setContent(contentEleText);
 					articleVo.setContentText(contentEleText);
@@ -83,8 +83,12 @@ public class TArticleController extends Controller {
 				articleVo.setContentText(contentEleText);
 			}
 
-		} catch (Exception e) {
-			System.out.println("页面为空");
+		} catch (UnsupportedMimeTypeException e) {
+			System.out.println("<error> 网页类型不正确");
+		} catch (UnsupportedEncodingException e) {
+			System.out.println("<error> 编码不正确");
+		} catch (IOException e) {
+			System.out.println("<error> 网址无法读取");
 		}
 		return articleVo;
 	}
@@ -205,18 +209,20 @@ public class TArticleController extends Controller {
 	}
 
 	private List<ArticleVo> getArticleList(int offset) {
-		System.out.println("当前抓取的offset为：" + offset);
-		System.out.println("当前抓取的cacId为：" + cacId);
+		System.out.println("");
+		System.out.println("-------------------------------------------------------------------------------------------------------------------------------------------------");
+		
 		List<ArticleVo> arts = new ArrayList<ArticleVo>();
 		Document doc;
 
 		try {
 			String weburl = baseUrl + "?offset=" + offset + "&cacId=" + cacId;
-			System.out.println("请求的URL为：" + weburl);
+			System.out.println("请求的列表页的URL为：" + weburl);
+			System.out.println("");
+			
 			doc = Jsoup.connect(weburl).get();
 			String baseUrl = doc.baseUri();
-			Elements listOuterEle = doc.getElementsByAttributeValue("class",
-					"xin2zuo");
+			Elements listOuterEle = doc.getElementsByAttributeValue("class", "xin2zuo");
 			Elements trEles = listOuterEle.get(0).getElementsByTag("tr");
 			for (int i = trEles.size() - 1; i >= 0; i--) {
 				Element ele = trEles.get(i);
@@ -233,6 +239,7 @@ public class TArticleController extends Controller {
 					articleVo.setTitle(title);
 					articleVo.setAddDate(addDate);
 					articleVo.setUrl(url);
+					System.out.println(String.format("(%s)抓取的详情页URL:%s", arts.size()+1, articleVo.getUrl()));
 					articleVo = getDetail(articleVo);
 					if (!articleVo.getContent().equals("")) {
 						arts.add(articleVo);
@@ -260,32 +267,16 @@ public class TArticleController extends Controller {
 		List<String> sqlList = new ArrayList<String>();
 		for (ArticleVo articleVo : artList) {
 
-			String sql = String
-					.format("insert into WCMDOCUMENT "
-							+ "(DOCID,DOCCHANNEL,DOCTYPE,DOCTITLE,DOCSECURITY,DOCSTATUS,DOCKIND,DOCCONTENT,"
-							+ "DOCHTMLCON,DOCEDITOR,DOCOUTUPID,DOCRELTIME,CRUSER,CRTIME,DOCWORDSCOUNT,DOCPUBHTMLCON,"
-							+ "ATTACHPIC,OPERTIME,OPERUSER,DOCSOURCENAME,SITEID,SRCSITEID,DOCFORM,DOCLEVEL)"
-							+ " VALUES"
-							+ " (%s,%s,%s,'%s',%s,%s,%s,'%s',"
-							+ "'%s','%s',%s,to_date('%s','yyyy-MM-dd'),'%s',%s,%s,'%s',"
-							+ "%s,%s,'%s','%s',%s,%s,%s,%s)",
-							"(select max(DOCID)+1 from WCMDOCUMENT)", 101, 20,
-							articleVo.getTitle(), 0, 1, 0,
-							articleVo.getContentText(), articleVo.getContent(),
-							"admin", 0, articleVo.getAddDate(), "admin",
-							"sysdate", articleVo.getContentText().length(),
-							articleVo.getContent(), 0, "sysdate", "admin",
-							articleVo.getSource(), 4, 4, 1, 1);
+			String sql = String.format("insert into WCMDOCUMENT " + "(DOCID,DOCCHANNEL,DOCTYPE,DOCTITLE,DOCSECURITY,DOCSTATUS,DOCKIND,DOCCONTENT,"
+					+ "DOCHTMLCON,DOCEDITOR,DOCOUTUPID,DOCRELTIME,CRUSER,CRTIME,DOCWORDSCOUNT,DOCPUBHTMLCON," + "ATTACHPIC,OPERTIME,OPERUSER,DOCSOURCENAME,SITEID,SRCSITEID,DOCFORM,DOCLEVEL)"
+					+ " VALUES" + " (%s,%s,%s,'%s',%s,%s,%s,'%s'," + "'%s','%s',%s,to_date('%s','yyyy-MM-dd'),'%s',%s,%s,'%s'," + "%s,%s,'%s','%s',%s,%s,%s,%s)",
+					"(select max(DOCID)+1 from WCMDOCUMENT)", pid, 20, articleVo.getTitle(), 0, 1, 0, articleVo.getContentText(), articleVo.getContent(), "admin", 0, articleVo.getAddDate(), "admin",
+					"sysdate", articleVo.getContentText().length(), articleVo.getContent(), 0, "sysdate", "admin", articleVo.getSource(), 4, 4, 1, 1);
 
-			String sql2 = String
-					.format("insert into WCMCHNLDOC "
-							+ "(CHNLID,DOCID,DOCSTATUS,CRUSER,CRTIME,DOCRELTIME,SITEID,SRCSITEID,DOCFORM,DOCLEVEL,ATTACHPIC,POSCHNLID,RECID,DOCCHANNEL,OPERUSER,OPERTIME)"
-							+ " VALUES"
-							+ " (%s,%s,%s,'%s',%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,'%s',%s)",
-							101, "(select max(DOCID) from WCMDOCUMENT)", 1,
-							"admin", "sysdate", "sysdate", 4, 4, 1, 1, 0, 101,
-							"(select max(RECID)+1 from WCMCHNLDOC)", 101,
-							"admin", "sysdate");
+			String sql2 = String.format("insert into WCMCHNLDOC "
+					+ "(CHNLID,DOCID,DOCSTATUS,CRUSER,CRTIME,DOCRELTIME,SITEID,SRCSITEID,DOCFORM,DOCLEVEL,ATTACHPIC,POSCHNLID,RECID,DOCCHANNEL,OPERUSER,OPERTIME)" + " VALUES"
+					+ " (%s,%s,%s,'%s',%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,'%s',%s)", pid, "(select max(DOCID) from WCMDOCUMENT)", 1, "admin", "sysdate", "sysdate", 4, 4, 1, 1, 0, pid,
+					"(select max(RECID)+1 from WCMCHNLDOC)", pid, "admin", "sysdate");
 
 			sqlList.add(sql);
 			sqlList.add(sql2);
@@ -310,6 +301,26 @@ public class TArticleController extends Controller {
 	public void listRange() {
 		int beginOffset = getParaToInt("beginOffset");
 		int endOffset = getParaToInt("endOffset");
+		if (endOffset > beginOffset) {
+			int tempOffset = beginOffset;
+			beginOffset = endOffset;
+			endOffset = tempOffset;
+		}
+		this.cacId = getPara("cacId");
+		fileBasePath = ZJ_FileUtils.getBaseFilePath(getRequest());
+		List<ArticleVo> arts = getArticleList(beginOffset, endOffset);
+		String sqls = getSql(arts);
+		sqls += ";\ncommit;";
+		sqls = "Set define off;\n" + sqls;
+		renderText(sqls);
+
+	}
+	
+	public void listRange2() {
+		int beginPage = getParaToInt("beginPage");
+		int endPage = getParaToInt("endPage");
+		int beginOffset = (beginPage-1)*20;
+		int endOffset = (endPage-1)*20;
 		if (endOffset > beginOffset) {
 			int tempOffset = beginOffset;
 			beginOffset = endOffset;
